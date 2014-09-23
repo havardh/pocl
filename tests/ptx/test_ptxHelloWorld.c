496555b983520bd952e1cc774d76feb2a1a47846
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2014-09-23 10:42:52 +0200

    Added test to confirm that the ptx backend is hooked up

---
+++ tests/ptx/test_ptxHelloWorld.c
@@ -0,0 +1,23 @@
+#include "poclu.h"
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+  cl_context ctx;
+  cl_device_id did;
+  cl_command_queue queue;
+  cl_int err;
+  size_t rvs;
+  char result[1024];
+
+  poclu_get_any_device( &ctx, &did, &queue );
+
+  err = clGetDeviceInfo( did, CL_DEVICE_NAME, sizeof(result), result, &rvs);
+
+  result[rvs] = '\0';
+
+  printf("%s\n", result);
+
+  return 0;
+}
